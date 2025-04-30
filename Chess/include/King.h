a00fa7ab6d5a99a76dfc4dbe08435ddf095f0efa
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2025-04-30 23:06:09 +0300

    fixing king methods decleration

--- Chess/include/King.h
+++ Chess/include/King.h
@@ -10,7 +10,8 @@ class King : public Piece {
 public:
     King(bool isWhite); // constructor
     virtual bool isValidMove(int srcRow, int srcCol, int destRow, int destCol, const Board& board) const override; // check if the movement is valid
-    virtual bool isInCheck(int row, int col, const Board& board) const; // check if the king is in check
-    virtual bool isInCheckmate(int row, int col, const Board& board) const; // check if the king is in checkmate
+    bool isInCheck(int row, int col, const Board &board) const;
+    bool isInCheckmate(int row, int col, const Board &board) const;
+    bool isInStalemate(int row, int col, const Board &board) const;
 };
 #endif // KING_H
